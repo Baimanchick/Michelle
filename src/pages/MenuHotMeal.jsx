8e287835bf3c7532c0d0be9f3f494aa0cc53dc47
aibekovislam <islamxdaamduu@gmail.com>
2023-12-20 15:05:44 +0300

    dfg

--- src/pages/MenuHotMeal.jsx
+++ src/pages/MenuHotMeal.jsx
@@ -95,14 +95,9 @@ function MenuHotMeal() {
             price={"905 С"}
             weight={"400 г"}
             icon1={icon1} // пщеница
-            // icon2={icon2} // листок
             icon3={icon3}
-            //   icon4={icon4} повар
-            // icon5={icon5} // докрашенный листок
-            //   icon6={icon6} бицепс
             icon7={icon7} // без молока
-            // icon8={icon8} // авакадо
-            //   icon9={icon9} старбакс
+            style={{ top: '115px' }}
           />
           <CardSalad
             img={img3}
@@ -119,6 +114,7 @@ function MenuHotMeal() {
             // icon7={icon7} // без молока
             // icon8={icon8} // авакадо
             //   icon9={icon9} старбакс
+            style={{ top: '115px' }}
           />
           <CardSalad
             img={img4}
@@ -135,6 +131,7 @@ function MenuHotMeal() {
             // icon7={icon7} // без молока
             // icon8={icon8} // авакадо
             //   icon9={icon9} старбакс
+            style={{ top: '115px' }}
           />
           <CardSalad
             img={img5}
@@ -206,6 +203,7 @@ function MenuHotMeal() {
             // icon7={icon7} // без молока
             // icon8={icon8} // авакадо
             //   icon9={icon9} старбакс
+            style={{ top: '115px' }}
           />
           <CardSalad
             img={img9}
@@ -223,6 +221,7 @@ function MenuHotMeal() {
             //   icon6={icon6} бицепс
             // icon7={icon7} // без молока
             // icon8={icon8} // авакадо
+            style={{ top: '115px' }}
             icon9={icon9}
           />
           <img src={img10} alt="" className="dinner-image" />
@@ -243,6 +242,7 @@ function MenuHotMeal() {
             icon7={icon7} // без молока
             // icon8={icon8} // авакадо
             //   icon9={icon9} старбакс
+            style={{ top: '115px' }}
           />
           <CardSalad
             img={img12}
@@ -311,6 +311,7 @@ function MenuHotMeal() {
             // icon7={icon7} // без молока
             // icon8={icon8} // авакадо
             //   icon9={icon9} старбакс
+            style={{ top: '115px' }}
           />
           <CardSalad
             img={img16}
@@ -329,6 +330,7 @@ function MenuHotMeal() {
             // icon7={icon7} // без молока
             // icon8={icon8} // авакадо
             //   icon9={icon9} старбакс
+            style={{ top: '115px' }}
           />
           <CardSalad
             img={img17}
