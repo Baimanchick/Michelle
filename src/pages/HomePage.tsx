0b1474871ac5ba091f1ab03144dc5d53a784054f
Baimanchick <baika132213@gmail.com>
2024-07-05 18:04:46 +0600

    added vercel analytics

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import HomeLoader from '../components/HomeLoader/HomeLoader'
 import "../styles/main.scss"
+import { Analytics } from "@vercel/analytics/react";
 
 function HomePage() {
     return (
         <div className='main-image'>
             <HomeLoader />
+            <Analytics />
         </div>
     )
 }
